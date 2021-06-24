9203c8b2dc115165190ee7d37e8ed608694209c8
schujack-ctrl <86435255+jackrschumacher@users.noreply.github.com>
2021-06-24 16:14:29 -0500

    Add Info

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ SpaceExplorer
 
 user_name = str(input("Please enter your username:"))
 print("Your username is:",user_name)
+
+print("You are the captian of a Nebula-Class starship known as the U.S.S. Endurance")
+print("=========")
