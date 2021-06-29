fb3209b256c8b0eb5a85e6af1396e16e47944239
schujack-ctrl <86435255+jackrschumacher@users.noreply.github.com>
2021-06-29 16:59:56 -0500

    Add Water to Store

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ def game_start():
     user_input = str(input("What would you like to do?"))
     alive = True
 
+    if user_input == "Help" or user_input == "help" or user_input == "hlp":
+                print('''
+                List of Space Explorer Commands
+                -Travel
+                -Supplies
+                -Buy Ship
+                -Planet (Generate a list of planets)
+                -Mine
+                ''')
     while alive == True:
-        if user_input == "Help" or user_input == "help" or user_input == "hlp":
-            print('''
-            List of Space Explorer Commands
-            -Travel
-            -Supplies
-            -Buy Ship
-            -Planet (Generate a list of planets)
-            -Mine
-            ''')
-        elif user_input == "Travel" or user_input == "travel" or user_input == "trvl":
-            print("You have chosen to travel")
+        
+        if user_input == "Travel" or user_input == "travel" or user_input == "trvl":
             trvl_to = str(input("Where do you wish to travel? (Type List for a list of avaliable planets"))
             if trvl_to == "list" or trvl_to == "List" or trvl_to == "lst":
                 print('''
@@ -28,13 +28,12 @@ def game_start():
                 -Jupiter(Not explorable by humans-probes needed)
                 
                 ''')
-                start_planet = "Earth"
-            elif user_input == "Earth" or user_input == "earth":
+            elif trvl_to == "Earth" or trvl_to == "earth":
                 print("Your charachter Begins on Earth...")
                 user_input = str(input("Would You like to replenish your ship, shop for a new one, or more"))
-                if user_input == "skip" or user_input == "Skip":
+                if trvl_to == "skip" or trvl_to == "Skip":
                     print("You have chosen to skip")
-                elif user_input == "rep" or user_input == "Rep" or user_input == "Replenish" or user_input == "replinish":
+                elif trvl_to == "rep" or user_input == "Rep" or user_input == "Replenish" or user_input == "replinish":
                     print("=========")
                     print("You have chosen to replenish your ship")
                     print("=========")
@@ -42,9 +41,24 @@ def game_start():
                     print("=========")
                     print("You have chosen to shop for supplies.")
                     print("=========")
-                    item = str(input("What Item do you wish to shop for"))
-            else:
-                print("y")
+                    print("Type list for a list of store items")
+                    item = str(input("What Item do you wish to shop for: "))
+                    while quit = False:
+                        if item == "list" or item == "List" or item == "lst" or item == "Lst":
+                            print('''
+                            List of Store items
+                            =========
+                            -Water
+                            -Food
+                            -HyperFuel
+                            -Encoder
+                            ''')
+                            item = str(input("What Item do you wish to shop for: "))
+                        elif item == "Water" or item == "water" or item == "wtr":
+                            water_amnt = str(input("How much water do you wish to buy:"))
+                            print("You have chosen to buy:", water_amnt)
+                else:
+                    print("y")
 def main():
     intro()
     game_start()
